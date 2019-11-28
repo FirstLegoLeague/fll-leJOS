81a339f82ad037a8df5279d95294fcd46f97b965
johnmeshulam <johnmeshulam@gmail.com>
2019-11-28 19:25:57 +0200

    merge user files to one

---
+++ src/user/Startup.java
@@ -0,0 +1,18 @@
+package user;
+
+public class Startup {
+	
+	public static void createHardware() {
+		//declare your hardware here
+	}
+	
+	public static void createRuns() {
+		//declare your run here
+	}
+	
+	public static void init() {
+		//code that you want to run ONCE 
+		//before the main menu starts
+	}
+	
+}
